98e9f9a5add90899d1400e0c35364fa209d562e9
se316 <sanievans316@gmail.com>
2022-03-22 18:03:53 +0000

    Add https capability
    - added pyopenssl to app's requirements file
    - added ssl_context='adhoc' in main.py for self-signed certs and changed port to 443
    - updated docker-compose to redirect localhost https connections to the app.

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ def index():
     return redirect(url_for('view.home'))
 
 # run the server
-app.run(debug=True, host='0.0.0.0', port='8080')
+app.run(debug=True, host='0.0.0.0', port='443', ssl_context='adhoc')
